8899925432b6d9da4fa84b6b261ff841ac40c94f
Gabe Johnson <gajo2539@colorado.edu>
2024-11-14 15:07:12 -0700

    Added Jest and a test

--- src/hello.ts
+++ src/hello.ts
@@ -15,7 +15,7 @@ type Vector = {
     dy: number;
 }
 
-type Spaceship = {
+export type Spaceship = {
     position: Vector;
     velocity: Vector;
     accel: Vector;
@@ -35,3 +35,15 @@ const myShip: Spaceship = {
         dy: 0
     }
 }
+
+export function moveShip(ship: Spaceship, positionChange: Vector) {
+    ship.position = { dx: ship.position.dx + positionChange.dx, dy: ship.position.dy + positionChange.dy };
+}
+
+/*
+"@jest/globals": "^29.7.0",
+    "@types/jest": "^29.5.14",
+    "jest": "^29.7.0",
+    "ts-jest": "^29.2.5",
+    
+*/
